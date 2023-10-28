debdca64e41901613c538fc3cfce8ef380c8d479
Muhammad Irtaza Ghaffar <93589223+M-IrtazaGhaffar@users.noreply.github.com>
2023-10-29 01:30:07 +0500

    11

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import "./App.css";
 import "./styles/custom.css";
-import Login from './screens/Login';
+import Login from "./screens/Login";
 import Edit from "./screens/Edit";
-import Protected from './screens/Protected';
-import Settings from './screens/Settings';
-import Home from './screens/Home';
-import Dashboard from './screens/Dashboard';
-import Create from './screens/Create';
+import Protected from "./screens/Protected";
+import Settings from "./screens/Settings";
+import Home from "./screens/Home";
+import Dashboard from "./screens/Dashboard";
+import Create from "./screens/Create";
+import ChangePassword from "./screens/ChangePassword";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <h1 className='logo'>
-          <Link to='/dashboard/'>LessonOfIslam</Link>
+        <h1 className="logo">
+          <Link to="/dashboard/">LessonOfIslam</Link>
         </h1>
         <Routes>
-          <Route path='/*' element={<Login />} />
-          <Route exact path='/dashboard' element={<Protected Component={Dashboard} />}>
+          <Route path="/*" element={<Login />} />
+          <Route exact path="/changepassword/:token" element={<ChangePassword />} />
+          <Route
+            exact
+            path="/dashboard"
+            element={<Protected Component={Dashboard} />}
+          >
             <Route index element={<Home />} />
-            <Route exact path='/dashboard/edit' element={<Edit />} />
-            <Route exact path='/dashboard/settings' element={<Settings />} />
-            <Route exact path='/dashboard/create' element={<Create />} />
+            <Route exact path="/dashboard/edit" element={<Edit />} />
+            <Route exact path="/dashboard/settings" element={<Settings />} />
+            <Route exact path="/dashboard/create" element={<Create />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- src/screens/Edit.jsx
+++ src/screens/Edit.jsx
@@ -1,55 +1,55 @@
-import React, { useEffect, useMemo, useState } from 'react'
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import { useLocation, useNavigate } from 'react-router-dom';
-import Goback from '../components/Goback';
-import axios from 'axios';
-import { useDispatch, useSelector } from 'react-redux';
-import Loader from '../components/Loader';
-import { logout } from '../reducers/Reducers';
+import React, { useEffect, useMemo, useState } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { useLocation, useNavigate } from "react-router-dom";
+import Goback from "../components/Goback";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../components/Loader";
+import { logout } from "../reducers/Reducers";
 
 function Edit() {
-  const dispatch = useDispatch()
-  const location = useLocation()
-  const data = location.state?.data
-  const n = useNavigate()
-  const { uid, token } = useSelector(state => state.auth)
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const data = location.state?.data;
+  const n = useNavigate();
+  const { token } = useSelector((state) => state.auth);
 
-  const [editorData, setEditorData] = useState('');
-  const [Loading, setLoading] = useState(0)
-  const [Error, setError] = useState(0)
-  const [Title, setTitle] = useState('')
-  const [Descr, setDescr] = useState('')
-  const [Img, setImg] = useState('')
-  const [NewImg, setNewImg] = useState('')
-  const [Detail, setDetail] = useState('')
-  const [Tag, setTag] = useState([])
-  const [SelectedTag, setSelectedTag] = useState([])
+  const [editorData, setEditorData] = useState("");
+  const [Loading, setLoading] = useState(0);
+  const [Error, setError] = useState(0);
+  const [Title, setTitle] = useState("");
+  const [Descr, setDescr] = useState("");
+  const [Img, setImg] = useState("");
+  const [SelectedTag, setSelectedTag] = useState([]);
 
   const fetchBlog = async () => {
     try {
-      setLoading(1)
-      const res = await axios.post(`https://api-lessonofislam.ittidevelops.com/writer/fetchBlog`, {
-        token: token,
-        id: data
-      })
-      setTitle(await res.data.data.Title)
-      setDescr(await res.data.data.Descr)
-      setEditorData(await res.data.data.Detail)
-      setSelectedTag(res.data.data.tid)
-      setImg(await res.data.data.Image)
-      setLoading(0)
+      setLoading(1);
+      const res = await axios.post(
+        `https://api-lessonofislam.ittidevelops.com/writer/fetchBlog`,
+        {
+          token: token,
+          id: data,
+        }
+      );
+      setTitle(await res.data.data.Title);
+      setDescr(await res.data.data.Descr);
+      setEditorData(await res.data.data.Detail);
+      setSelectedTag(res.data.data.tid);
+      setImg(await res.data.data.Image);
+      setLoading(0);
     } catch (error) {
       console.log(error);
-      alert('Some Error Ocurred')
-      setError(1)
+      alert("Some Error Ocurred");
+      setError(1);
     }
-  }
+  };
 
   const UploadData = async (e) => {
     e.preventDefault();
 
-    setLoading(1)
+    setLoading(1);
 
     try {
       // const res = await axios.post('https://api-lessonofislam.ittidevelops.com/writer/editBlog', {
@@ -61,112 +61,131 @@ function Edit() {
       //   detail: editorData,
       //   tid: Number(SelectedTag)
       // })
-      const res = await axios.post('http://localhost:5000/writer/editBlog', {
+      const res = await axios.post("http://localhost:5000/writer/editBlog", {
         token: token,
         id: data,
         title: Title,
         descr: Descr,
         detail: editorData,
-      })
-      alert(await res.data.data)
-      setDescr('')
-      setTitle('')
-      setEditorData('')
-      setSelectedTag('')
-      n('/dashboard/')
+      });
+      alert(await res.data.data);
+      setDescr("");
+      setTitle("");
+      setEditorData("");
+      setSelectedTag("");
+      n("/dashboard/");
     } catch (error) {
       console.log(error);
-      setError(1)
-      let err = error.response.status || 500
+      setError(1);
+      let err = error.response.status || 500;
       if (err === 401) {
-        alert(error.response.data.message)
-        dispatch(logout())
-      }
-      else alert('Some Error Occured')
+        alert(error.response.data.message);
+        dispatch(logout());
+      } else alert("Some Error Occured");
       return;
     }
-    setLoading(0)
-  }
+    setLoading(0);
+  };
 
   useEffect(() => {
-    fetchBlog()
-  }, [])
-
+    fetchBlog();
+  }, []);
 
   return (
-    <div className='create'>
+    <div className="create">
       <Goback />
-      <div className='route'>
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-list-nested" viewBox="0 0 16 16">
-          <path fillRule="evenodd" d="M4.5 11.5A.5.5 0 0 1 5 11h10a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zm-2-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm-2-4A.5.5 0 0 1 1 3h10a.5.5 0 0 1 0 1H1a.5.5 0 0 1-.5-.5z" />
+      <div className="route">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          fill="currentColor"
+          className="bi bi-list-nested"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fillRule="evenodd"
+            d="M4.5 11.5A.5.5 0 0 1 5 11h10a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zm-2-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm-2-4A.5.5 0 0 1 1 3h10a.5.5 0 0 1 0 1H1a.5.5 0 0 1-.5-.5z"
+          />
         </svg>
         <h3>Edit</h3>
       </div>
-      {
-        Error
-          ?
-          <p className='error'>Some Error Occured</p>
-          :
-          Loading
-            ?
-            <Loader />
-            :
-            <div>
-              {
-                data ?
-                  <form onSubmit={UploadData}>
-                    <div>
-                      <p>Blog ID #{location.state.data}</p>
-                      <p>Publish Date #10-09-2023</p>
-                    </div>
-                    <div>
-                      <label htmlFor="title">Title</label>
-                      <textarea rows={5} name='title' value={Title} onChange={(e) => setTitle(e.target.value)} maxLength={250} required></textarea>
-                      <p className='length'>{`${Title.length}/250`}</p>
-                    </div>
-                    <span>
-                      <label htmlFor="description">Description</label>
-                      <textarea rows={10} name='description' value={Descr} onChange={(e) => setDescr(e.target.value)} maxLength={250} required></textarea>
-                      <p className='length'>{`${Descr.length}/250`}</p>
-                    </span>
-                    <p style={{ color: 'red' }}>
-                      Sorry! But we don't allow to change the tags and Images
-                    </p>
-                    <span>
-                      <p>Uploaded Image</p>
-                      <img src={Img} alt="image" width={500} />
-                    </span>
-                    <span>
-                      <p>Start writing from here</p>
-                      <CKEditor
-                        editor={ClassicEditor}
-                        data={editorData}
-                        onReady={editor => {
-                          // You can store the "editor" and use when it is needed.
-                          // console.log('Editor is ready to use!', editor);
-                        }}
-                        onChange={(event, editor) => {
-                          const data = editor.getData();
-                          setEditorData(data)
-                        }}
-                        onBlur={(event, editor) => {
-                          // console.log('Blur.', editor);
-                        }}
-                        onFocus={(event, editor) => {
-                          // console.log('Focus.', editor);
-                        }}
-                      />
-                      <p className='length'>{`${editorData.length}`}</p>
-                    </span>
-                    <button className='btn' type='submit'>Upload</button>
-                  </form>
-                  :
-                  <p>No data to show right now!</p>
-              }
-            </div>
-      }
+      {Error ? (
+        <p className="error">Some Error Occured</p>
+      ) : Loading ? (
+        <Loader />
+      ) : (
+        <div>
+          {data ? (
+            <form onSubmit={UploadData}>
+              <div>
+                <p>Blog ID #{location.state.data}</p>
+                <p>Publish Date #10-09-2023</p>
+              </div>
+              <div>
+                <label htmlFor="title">Title</label>
+                <textarea
+                  rows={5}
+                  name="title"
+                  value={Title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  maxLength={250}
+                  required
+                ></textarea>
+                <p className="length">{`${Title.length}/250`}</p>
+              </div>
+              <span>
+                <label htmlFor="description">Description</label>
+                <textarea
+                  rows={10}
+                  name="description"
+                  value={Descr}
+                  onChange={(e) => setDescr(e.target.value)}
+                  maxLength={250}
+                  required
+                ></textarea>
+                <p className="length">{`${Descr.length}/250`}</p>
+              </span>
+              <p style={{ color: "red" }}>
+                Sorry! But we don't allow to change the tags and Images
+              </p>
+              <span>
+                <p>Uploaded Image</p>
+                <img src={Img} alt="image" width={500} />
+              </span>
+              <span>
+                <p>Start writing from here</p>
+                <CKEditor
+                  editor={ClassicEditor}
+                  data={editorData}
+                  onReady={(editor) => {
+                    // You can store the "editor" and use when it is needed.
+                    // console.log('Editor is ready to use!', editor);
+                  }}
+                  onChange={(event, editor) => {
+                    const data = editor.getData();
+                    setEditorData(data);
+                  }}
+                  onBlur={(event, editor) => {
+                    // console.log('Blur.', editor);
+                  }}
+                  onFocus={(event, editor) => {
+                    // console.log('Focus.', editor);
+                  }}
+                />
+                <p className="length">{`${editorData.length}`}</p>
+              </span>
+              <button className="btn" type="submit">
+                Upload
+              </button>
+            </form>
+          ) : (
+            <p>No data to show right now!</p>
+          )}
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Edit
+export default Edit;
